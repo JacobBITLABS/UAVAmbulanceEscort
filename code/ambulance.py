544cc924a67540cc9f92957855f07aad0e2ab8e0
Niller <nielsproest@hotmail.com>
2023-01-09 19:14:59 +0100

    default position for ambulance

--- code/ambulance.py
+++ code/ambulance.py
@@ -1,7 +1,8 @@
+from MAVFleetControl.mavfleetcontrol.states.position import Position
 
 class Ambulance:
     """
     Class Responsible for contact with Drones via the MavLink Message Protocol
     """
     def __init__(self):
-        self.position = None
+        self.position = Position(47.3977419, 8.5455937, 488+1)
